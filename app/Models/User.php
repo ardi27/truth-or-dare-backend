59d81ebf1e5ca207748a59cd52e8d19c76303fa2
ardiprima_ <ardiprima27@gmail.com>
2021-02-21 01:10:00 +0700

    uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\UsesUuid;
+use App\Http\Traits\UsesUuid;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
