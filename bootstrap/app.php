7f0b3a669cdf285315b837428a19c425756eb779
ardiprima_ <ardiprima27@gmail.com>
2021-02-20 10:39:50 +0700

    proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,9 +74,9 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\TrustProxies::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
