2030abea6268d01481b6afff17196730d02c042e
ardiprima_ <ardiprima27@gmail.com>
2021-02-24 23:09:52 +0700

    Update password

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Utils\ResponseTemplate;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
@@ -40,5 +44,34 @@ class UsersController extends Controller
             $res['message'] = "Profile succesfully updated";
         }
         return response()->json($res, $res['code']);
+    }
+    public function updatePassword(Request $request)
+    {
+        $res = ResponseTemplate::getResponse();
+        $validator = Validator::make($request->all(), [
+            'old_password' => 'required',
+            'new_password' => 'required'
+        ]);
+        if ($validator->fails()) {
+            $res['code'] = 422;
+            $res['results'] = $validator->errors()->first();
+            $res['message'] = "Form validation error";
+            return response()->json($res, $res['code']);
+        }
+        try {
+            $user = User::findOrFail($request->auth->uuid);
+            if (Hash::check($request->old_password, $user->password)) {
+                $user->password = Hash::make($request->new_password);
+                $user->save();
+                $res['code'] = 200;
+                $res['message'] = 'Password succesfully updated';
+            } else {
+                $res['code'] = 404;
+                $res['message'] = "Password not match";
+            }
+            return response()->json($res, $res['code']);
+        } catch (Exception $e) {
+            return response()->json($res, $res['code']);
+        }
     }
 }
